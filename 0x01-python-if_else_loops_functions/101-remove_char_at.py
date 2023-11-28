3a88f3a130039033c639b85f9fb26a4dbd81beea
namycodes <namycodes@yahoo.com>
2023-11-28 14:57:22 -0800

    Gone

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def remove_char_at(str, n):
+    if 0 <= n < len(s):
+        result = ""
+        for i in range(len(S)):
+            if i != n:
+                result += s[i]
+        return result
+    else:
+        return s
