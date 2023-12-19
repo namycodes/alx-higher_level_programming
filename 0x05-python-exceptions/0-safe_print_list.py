af0b76d1459083b93e10fdfea668edbc0cd4e197
namycodes <namycodes@yahoo.com>
2023-12-19 04:45:46 -0800

    Fized issue again

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,6 +2,8 @@
 def safe_print_list(my_list=[], x=0):
     try:
         for i in range(0, x):
-            print("{:d}".format(my_list[i]), end="")
-    except Exception as b:
-        print("Error", b)
+            print("{:}".format(my_list[i]), end="")
+        print("")
+        return my_list[-1]
+    except:
+        print("")
