48ddb5ac979ad71446c269136228344354df7509
namycodes <namycodes@yahoo.com>
2023-12-20 06:09:00 -0800

    Made Updates

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""
+This module defines a class called Square
+And also instantiates a private attribute called size
+"""
+
+
+class Square:
+    """
+    The __init__ module is the instance of the class
+    It is the first module to run when a class is called
+    """
+    def __init__(self, __size):
+        self.size = __size
